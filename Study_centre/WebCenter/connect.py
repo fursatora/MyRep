b8449a6e22894439b290c74847f3c78a5bbbcd6c
Виктория Фурса <vikahomeacc@gmail.com>
2024-10-10 02:16:40 +0300

    создание подключения к бд

---
+++ Study_centre/WebCenter/connect.py
@@ -0,0 +1,17 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
+conn = pymysql.connect(
+      host="rc1d-kk2w92q1pk857nd6.mdb.yandexcloud.net",
+      port=3306,
+      db="dbcenter",
+      user="fursatora",
+      passwd="fursatora",
+      ssl={'ca': '~/.mysql/root.crt'})
+
+cur = conn.cursor()
+cur.execute('SELECT version()')
+
+print(cur.fetchone()[0])
+
+conn.close()
