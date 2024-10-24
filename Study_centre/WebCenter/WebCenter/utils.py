9f1406522b0e2f960a6b755b6f58907c27e825b9
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2024-10-24 14:48:51 +0300

    Update utils.py

--- Study_centre/WebCenter/WebCenter/utils.py
+++ Study_centre/WebCenter/WebCenter/utils.py
@@ -1,15 +1,3 @@
 from yandexcloud import SDK
-from yandex.cloud.secret_manager.v1 import SecretServiceClient
-from yandex.cloud.secret_manager.v1 import GetSecretRequest
 from yandex.cloud.exceptions import NotFoundError
 
-def get_secret(secret_name):
-    sdk = SDK()
-    secret_service_client = sdk.client(SecretServiceClient)
-
-    try:
-        request = GetSecretRequest(secret_id=secret_name)
-        secret = secret_service_client.get(request)
-        return secret.data.payload.decode()
-    except NotFoundError:
-        raise Exception(f'Secret {secret_name} not found.')
