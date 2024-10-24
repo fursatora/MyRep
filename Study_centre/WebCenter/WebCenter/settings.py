654622349601f71b6d4a88d076c7b96177f8d657
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2024-10-24 15:20:46 +0300

    Update settings.py

--- Study_centre/WebCenter/WebCenter/settings.py
+++ Study_centre/WebCenter/WebCenter/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.47.1','fursatora_study_center.com' ]
+ALLOWED_HOSTS = ['130.193.44.11','fursatora_study_center.com' ]
 
 
 # Application definition
