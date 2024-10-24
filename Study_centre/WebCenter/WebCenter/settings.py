b1f637f8c2df7c4815ca65db5c2dbbec05f99bbc
Виктория Фурса <vikahomeacc@gmail.com>
2024-10-24 16:42:46 +0300

    Revert "Update settings.py"
    This reverts commit 8268a311a1cc74e706ea92a2da0b8fde6552cf10.

--- Study_centre/WebCenter/WebCenter/settings.py
+++ Study_centre/WebCenter/WebCenter/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-8mb3xv8i86dja1y5!oxj3s1u%9-y7!4km)ah)li_u6f&v(8ei-'
+SECRET_KEY = 'django-insecure-8mb3xv8i86dja1y5!oxj3s1u%9-y7!4km)ah)li_u6f&v(8ei-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
